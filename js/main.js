dd79c52e1bdbc6ba35097f934f92f27d4e2eadfb
Olena <filchak.elena@gmail.com>
2019-05-19 11:32:18 +0300

    startpage-mobile

--- js/main.js
+++ js/main.js
@@ -23,3 +23,64 @@
 function GoToChallengePage(adress){
 window.open(adress);
 }
+
+// Start Modal Window
+var registerBg = document.getElementById('startModalBg');
+var accountBtn = document.getElementById('accountBtn');
+var registerClose = document.getElementsByClassName('startModalContent__close')[0];
+var startModalContent = document.getElementsByClassName('startModalContent')[0];
+
+accountBtn.onclick = function() { 
+	startModalBg.style.display = 'block'; 
+}
+
+registerClose.onclick = function() { 
+	startModalBg.style.display = 'none';
+}
+
+// registerBg.onclick = function() { 
+
+// 	registerBg.style.display = 'none';
+
+// }
+
+// Enter Modal Window
+var enterBg = document.getElementById('enterModalBg');
+var enterBtn = document.getElementsByClassName('startModalContent__enter')[0];
+var enterClose = document.getElementsByClassName('enterModalContent__close')[0];
+
+enterBtn.onclick = function() { 
+	registerBg.style.display = 'none'; 
+	enterBg.style.display = 'block';
+
+}
+
+enterClose.onclick = function() { 
+	enterModalBg.style.display = 'none';
+}
+
+window.onclick = function(event) { 
+	if (event.target == enterModalBg) { 
+		enterModalBg.style.display = 'none';
+	}
+}
+
+// Register Modal Window
+
+var menuBg = document.getElementById('menuBg');
+var menuBtn = document.getElementById('menuBtn');
+var menuClose = document.getElementsByClassName('menu__close')[0];
+
+menuBtn.onclick = function() { 
+	menuBg.style.display = 'block'; 
+}
+
+menuClose.onclick = function() { 
+	menuBg.style.display = 'none';
+}
+
+window.onclick = function(event) { 
+	if (event.target == menuBg) { 
+		menuBg.style.display = 'none';
+	}
+}
