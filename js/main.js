cbd49b7760cc6a80a359d5a85da3e5f513817685
Margarita <margomimi82@gmail.com>
2019-05-19 10:43:22 +0300

    about mobile

--- js/main.js
+++ js/main.js
@@ -3,6 +3,23 @@
 (function($){
 	$(document).ready(function() {
 		// Code
-
+		$('.slider').slick({
+			centerPadding: '60px',
+			slidesToShow: 3,
+			centerMode: true,
+  			slidesToScroll: 1,               
+  			dots: false,
+  			arrows:false,
+			infinite: true, 
+			variableWidth: true,  
+			
+		});
 	});
+	$(".button-do").click(function(){
+		GoToChallengePage("challenges.html")
+	})
 })(jQuery);
+
+function GoToChallengePage(adress){
+window.open(adress);
+}
