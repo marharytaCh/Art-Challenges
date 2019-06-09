6ead819db850c372541f2892b6be359569580906
Olena <filchak.elena@gmail.com>
2019-06-09 17:11:25 +0300

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -41,10 +41,11 @@ var paths = {
 var sources = {
 	css: [
 		'./node_modules/bootstrap/dist/css/bootstrap.min.css',
+
 		'./node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.css',
-		
-		'./app/css/libs/slick.css',
-		'./app/css/libs/slick-theme.css'
+
+		'./app/js/libs/slick/slick.css',
+		'./app/js/libs/slick/slick-theme.css'
 	],
 	js: [
 		'./node_modules/jquery/dist/jquery.min.js',
